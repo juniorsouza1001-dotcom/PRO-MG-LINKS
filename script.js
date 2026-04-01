5726fd5eb10ac33b80ba62795837932acb709de1
juniorsouza1001 <juniorsouza1001@gmail.com>
2026-04-01 08:18:59 +0100

    add all updates

--- script.js
+++ script.js
@@ -9,11 +9,26 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
     // tem que editar a foto no PS para cortar as bordas pretas
-    img.setAttribute("src", "./assets/jravatar oculos2.png")
-    img.setAttribute(
-      "alt", "Foto do Gil Jr sorrindo, usando uma camisa preta, óculos, barba e fundo branco.")
+    img.setAttribute("src", "./assets/mgblackavatar.png")
+    img.setAttribute("alt", "logo black")
   } else {
     //se tiver sem lightmode, manter a imagem normal
-    img.setAttribute("src", "./assets/jravatar.png")
+    img.setAttribute("src", "./assets/mgavatar.png")
   }
+}
+
+const tiktokLink = document.getElementById("tiktok-link")
+if (tiktokLink) {
+  tiktokLink.addEventListener("click", function (event) {
+    event.preventDefault()
+    alert("Link do TikTok em desenvolvimento 🙌")
+  })
+}
+
+const websiteLink = document.getElementById("website-link")
+if (websiteLink) {
+  websiteLink.addEventListener("click", function (event) {
+    event.preventDefault()
+    alert("Link do Website em desenvolvimento 🙌")
+  })
 }
